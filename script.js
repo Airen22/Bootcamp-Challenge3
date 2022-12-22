b1b52999d07ffb0501fccb47f541615b1940f0ad
Erin <erinpeifer@gmail.com>
2022-12-21 20:13:30 -0700

    added starter code from UTA repo for html, css, and js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-// Assignment code
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
 
-// Add event listener to genrate button
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
