83f7c93e9568abc3348d478257bd259d87c2017e
Erin <erinpeifer@gmail.com>
2022-12-22 22:40:03 -0700

    random generator from arrays for compChoice

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
 // User chooses length
-    function chooseLength () {
+    // function chooseLength () {
     var lengthChoice = prompt("How many characters? (8-128)");
         lengthChoice = parseInt(lengthChoice);
-// Validates that chosen lenght is in range
+// Validates that chosen length is in range
     function validateLengthChoice() {
         if (lengthChoice >= 8 && lengthChoice <=128) {
        console.log(lengthChoice);
@@ -28,10 +28,11 @@ function generatePassword(){
         }
       };
       validateLengthChoice();
-        }
-    chooseLength();
+        // }
+    // chooseLength();
+
 // User chooses characters to include in PW
-    function chooseChars () {
+    // function chooseChars () {
     var upperConfirm = confirm("Include uppercase letters? (A, B, C, D, etc.)");
     console.log(upperConfirm);
     var lowerConfirm = confirm("Include lowercase letters? (a, b, c, d, etc.)");
@@ -46,84 +47,52 @@ function generatePassword(){
         return ;
         } else {
           alert("Invalid entry! Must select at least one character type.");
-          chooseChars();
+        //   chooseChars();
         }
       };
       validateCharacterChoice();   
-    }
-    chooseChars();
+    // }
+    // chooseChars();
     
 // Array for each character type
+// let upper = [];
+// let lower = [];
+// let num = [];
+// let sym = [];
+//    function randomComp (length) {
+    var compChoice = [];
+    function randomComp () {
+        if(upperConfirm == true){
+             var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+            var indexUpper = Math.floor(Math.random()*upper.length);
+            var upperChoice = upper[indexUpper];
+        }
+        if (lowerConfirm == true) {
+            var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+            var indexLower = Math.floor(Math.random()*lower.length);
+            var lowerChoice = lower[indexLower];    
+        }
+        if (numComfirm == true) {
+            var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+            var indexNum = Math.floor(Math.random()*num.length);
+            var numChoice = num[indexNum]; 
+        }
+        if (symConfirm == true) {
+            var sym = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", '+', "-", ".", "~", "|", "<", ">", "=", "-", "_", "/", ":", ";", "?", "[", "]", "{", "}"];
+            var indexSym = Math.floor(Math.random()*sym.length);
+            var symChoice = sym[indexSym];
+        }   
+            compChoice.push(upperChoice)
+            compChoice.push(lowerChoice)
+            compChoice.push(numChoice)
+            compChoice.push(symChoice)
+            console.log(compChoice)
+        }
+    for (var i = 0; i <= lengthChoice*.25; i++)
+    randomComp(i);
 
-const upper ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-function generateUpper(length) {
-    let resultUpper = ' ';
-    const upperLength = upper.length;
-    for ( let i = 0; i < length; i++ ) {
-        resultUpper += upper.charAt(Math.floor(Math.random() * upperLength));
-    }
-
-    return resultUpper;
-}
-console.log(generateUpper(120));
-
-const lower ='abcdefghijklmnopqrstuvwxyz';
-
-function generateLower(length) {
-    let resultLower = ' ';
-    const lowerLength = lower.length;
-    for ( let i = 0; i < length; i++ ) {
-        resultLower += lower.charAt(Math.floor(Math.random() * lowerLength));
-    }
-
-    return resultLower;
-
-}
-console.log(generateLower(120));
-
-const num ='0123456789';
-
-function generateNum(length) {
-    let resultNum = ' ';
-    const numLength = num.length;
-    for ( let i = 0; i < length; i++ ) {
-        resultNum += num.charAt(Math.floor(Math.random() * numLength));
-    }
-
-    return resultNum;
-
-}
-console.log(generateNum(120));
-
-const sym ='!@#$%^&*()<>?:;{}[]|~`';
-
-function generateSym(length) {
-    let resultSym = ' ';
-    const symLength = sym.length;
-    for ( let i = 0; i < length; i++ ) {
-        resultSym += sym.charAt(Math.floor(Math.random() * symLength));
-    }
-
-    return resultSym;
-
-}
-console.log(generateSym(120));
-
-    // function randomChoice () {
-    // var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-    // function upperComp (length) {
-    //     var uppercomp = "";
-    //     var indexUpper = Math.floor(Math.random()*upper.length);
-    // var upperChoice = upper[indexUpper];
-
-    // alert(upperChoice);
-    // var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    // var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    // var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", '+', "-", ".", "~", "|", "<", ">", "=", "-", "_", "/", ":", ";", "?", "[", "]", "{", "}"];
-    // }
-    // randomChoice()
-
+ 
+ 
 
     // must return a string value that is the password.
     // prompt user to choose num b/w 8 and 128
