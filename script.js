8f045251b101d69df97fe094200b39d88d80ad27
Erin <erinpeifer@gmail.com>
2022-12-24 11:18:00 -0700

    added to JS

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -15,7 +14,6 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
 // User chooses length
-    // function chooseLength () {
     var lengthChoice = prompt("How many characters? (8-128)");
         lengthChoice = parseInt(lengthChoice);
 // Validates that chosen length is in range
@@ -24,66 +22,66 @@ function generatePassword(){
        console.log(lengthChoice);
         } else {
           alert("Invalid entry! Must select number between 8-128.");
-          chooseLength();
         }
       };
       validateLengthChoice();
-        // }
-    // chooseLength();
 
 // User chooses characters to include in PW
-    // function chooseChars () {
+    var charChoice = [];
+
     var upperConfirm = confirm("Include uppercase letters? (A, B, C, D, etc.)");
     console.log(upperConfirm);
     var lowerConfirm = confirm("Include lowercase letters? (a, b, c, d, etc.)");
     console.log(lowerConfirm);
-    var numComfirm = confirm("Include numbers? (1, 2, 3, 4, etc.)");
-    console.log(numComfirm);
+    var numConfirm = confirm("Include numbers? (1, 2, 3, 4, etc.)");
+    console.log(numConfirm);
     var symConfirm = confirm("Include symbols? (!, @, #, $, etc.)");
     console.log(symConfirm);
+
 // Validates that at least one character type is chosen
     function validateCharacterChoice() {
-        if (upperConfirm == true || lowerConfirm == true || numComfirm == true || symConfirm == true) {
+        if (upperConfirm == true || lowerConfirm == true || numConfirm == true || symConfirm == true) {
         return ;
         } else {
           alert("Invalid entry! Must select at least one character type.");
         }
       };
       validateCharacterChoice();
+      
 //Computer chooses random characters   
     var compChoice = [];
     function randomComp () {
+        
         if(upperConfirm == true){
-             var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+            for (var i = 1; i <= lengthChoice; i++) {
+            var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
             var indexUpper = Math.floor(Math.random()*upper.length);
-            var upperChoice = upper[indexUpper]
-        }
+            var upperChoice = upper[indexUpper];
+            compChoice.push(upperChoice);
+            }
+        } 
         if (lowerConfirm == true) {
-            var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-            var indexLower = Math.floor(Math.random()*lower.length);
-            var lowerChoice = lower[indexLower];    
+            var poppedUpper = compChoice.pop ();
+            compChoice.unshift(poppedUpper.toLowerCase());
         }
-        if (numComfirm == true) {
+        if (numConfirm == true) {
             var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
             var indexNum = Math.floor(Math.random()*num.length);
-            var numChoice = num[indexNum]; 
+            var numChoice = num[indexNum];
+            poppedUpper = compChoice.pop();
+            compChoice.unshift(numChoice); 
         }
         if (symConfirm == true) {
             var sym = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", '+', "-", ".", "~", "|", "<", ">", "=", "-", "_", "/", ":", ";", "?", "[", "]", "{", "}"];
             var indexSym = Math.floor(Math.random()*sym.length);
             var symChoice = sym[indexSym];
+            poppedUpper = compChoice.pop();
+            compChoice.unshift(symChoice);
         }   
-            compChoice.push(upperChoice)
-            compChoice.push(lowerChoice)
-            compChoice.push(numChoice)
-            compChoice.push(symChoice)
-            compChoice.sort(() => Math.random() - 0.5);
+    
+           return compChoice.sort(() => Math.random() -0.5);
         }
-// ----------How to make it repeat until the chosen length is reached-------------
-    for (var i = 0; i <= lengthChoice*.25; i++)
-    randomComp();
-            console.log(compChoice.join(""))
-
+        var randomPassword = randomComp()
+        return randomPassword.join("")
     }
-
-// ----------How to call writepassword function-----------------    
+   
